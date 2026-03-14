ee8e0dc99f22a06211d799252e3a0aa6a19eb6ec
kushal kant bind <bindkushalkant@gmail.com>
2026-03-14 20:50:03 +0530

    fix: add get_palette and _MockPalette to ToolButton stub

--- sugar3_mock/graphics/toolbutton.py
+++ sugar3_mock/graphics/toolbutton.py
@@ -1,10 +1,17 @@
 from gi.repository import Gtk
 
+class _MockPalette:
+    def set_primary_text(self, text): pass
+    def get_primary_text(self): return ""
+    def popup(self): pass
+    def popdown(self): pass
+
 class ToolButton(Gtk.ToolButton):
     def __init__(self, icon_name=None, **kwargs):
         Gtk.ToolButton.__init__(self)
         if icon_name:
             self.set_icon_name(icon_name)
+        self._palette = _MockPalette()
 
     def set_tooltip(self, text):
         self.set_tooltip_text(text)
@@ -12,5 +19,8 @@ class ToolButton(Gtk.ToolButton):
     def set_named_icon(self, icon_name):
         self.set_icon_name(icon_name)
 
+    def get_palette(self):
+        return self._palette
+
     def create_palette(self):
-        return None
+        return self._palette
