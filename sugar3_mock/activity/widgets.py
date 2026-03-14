1943d464a5ee423048a3373dd73af6b78a257270
kushal kant bind <bindkushalkant@gmail.com>
2026-03-14 20:54:28 +0530

    fix: add is_expanded and set_expanded to ActivityToolbarButton stub

--- sugar3_mock/activity/widgets.py
+++ sugar3_mock/activity/widgets.py
@@ -1,19 +1,24 @@
-# sugar3_mock/activity/widgets.py
-import gi
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class ActivityToolbarButton(Gtk.ToolButton):
-    """Stub for the Sugar activity toolbar button (title + share etc.)."""
     def __init__(self, activity=None, **kwargs):
         Gtk.ToolButton.__init__(self)
         self.set_label('Activity')
         self._activity = activity
+        self._expanded = False
+
+    def is_expanded(self):
+        return self._expanded
+
+    def set_expanded(self, expanded):
+        self._expanded = expanded
+
+    def get_page(self):
+        return Gtk.Box()
 
 class StopButton(Gtk.ToolButton):
-    """Stub stop button - quits GTK main loop when clicked."""
     def __init__(self, activity=None, **kwargs):
         Gtk.ToolButton.__init__(self)
-        self.set_icon_name('process-stop')
-        self.set_tooltip_text('Stop')
-        self.connect('clicked', lambda _: Gtk.main_quit())
+        self.set_icon_name("process-stop")
+        self.set_tooltip_text("Stop")
+        self.connect("clicked", lambda _: Gtk.main_quit())
