5070599d933b9cd246f34b65b008e62cf197d335
kushal kant bind <bindkushalkant@gmail.com>
2026-03-14 20:55:30 +0530

    fix: add is_expanded to ToolbarButton stub

--- sugar3_mock/graphics/toolbarbox.py
+++ sugar3_mock/graphics/toolbarbox.py
@@ -1,22 +1,27 @@
-# sugar3_mock/graphics/toolbarbox.py
-import gi
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class ToolbarBox(Gtk.Box):
-    """Stub for sugar3 ToolbarBox - wraps a plain Gtk.Toolbar."""
     def __init__(self, **kwargs):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
         self.toolbar = Gtk.Toolbar()
         self.pack_start(self.toolbar, False, False, 0)
 
 class ToolbarButton(Gtk.ToolButton):
-    """Stub for sugar3 ToolbarButton (expandable toolbar button)."""
     def __init__(self, page=None, icon_name=None, **kwargs):
         Gtk.ToolButton.__init__(self)
         if icon_name:
             self.set_icon_name(icon_name)
         self._page = page or Gtk.Box()
+        self._expanded = False
+
+    def is_expanded(self):
+        return self._expanded
+
+    def set_expanded(self, expanded):
+        self._expanded = expanded
+
+    def set_tooltip(self, text):
+        self.set_tooltip_text(text)
 
     def get_page(self):
         return self._page
