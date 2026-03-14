243818efbedf118a755dc0e1a5895436fa195a18
kushal kant bind <bindkushalkant@gmail.com>
2026-03-14 20:49:08 +0530

    fix: add set_tooltip method to RadioToolButton and ToolButton stubs

--- sugar3_mock/graphics/radiotoolbutton.py
+++ sugar3_mock/graphics/radiotoolbutton.py
@@ -1,10 +1,6 @@
-# sugar3_mock/graphics/radiotoolbutton.py
-import gi
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class RadioToolButton(Gtk.RadioToolButton):
-    """Minimal RadioToolButton stub."""
     def __init__(self, icon_name=None, group=None, **kwargs):
         if group is not None:
             Gtk.RadioToolButton.__init__(self, group=group)
@@ -12,3 +8,9 @@ class RadioToolButton(Gtk.RadioToolButton):
             Gtk.RadioToolButton.__init__(self)
         if icon_name:
             self.set_icon_name(icon_name)
+
+    def set_tooltip(self, text):
+        self.set_tooltip_text(text)
+
+    def set_named_icon(self, icon_name):
+        self.set_icon_name(icon_name)
--- sugar3_mock/graphics/toolbutton.py
+++ sugar3_mock/graphics/toolbutton.py
@@ -1,14 +1,16 @@
-# sugar3_mock/graphics/toolbutton.py
-import gi
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class ToolButton(Gtk.ToolButton):
-    """Minimal ToolButton stub - just a plain GTK ToolButton."""
     def __init__(self, icon_name=None, **kwargs):
         Gtk.ToolButton.__init__(self)
         if icon_name:
             self.set_icon_name(icon_name)
+
+    def set_tooltip(self, text):
+        self.set_tooltip_text(text)
+
+    def set_named_icon(self, icon_name):
+        self.set_icon_name(icon_name)
 
     def create_palette(self):
         return None
