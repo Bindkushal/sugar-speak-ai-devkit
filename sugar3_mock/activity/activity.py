d4e7db726283e6ac51729322573bc22cdb810106
kushal kant bind <bindkushalkant@gmail.com>
2026-03-14 20:42:45 +0530

    fix: add Activity class to activity.py submodule

--- sugar3_mock/activity/activity.py
+++ sugar3_mock/activity/activity.py
@@ -14,3 +14,40 @@ def show_object_in_journal(object_id):
 def get_bundle_path():
     import os
     return os.path.dirname(os.path.abspath(__file__))
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GLib
+import os
+
+class Activity(Gtk.Window):
+    def __init__(self, handle=None):
+        Gtk.Window.__init__(self)
+        self.set_title('Speak AI (local)')
+        self.set_default_size(1200, 800)
+        self.connect('destroy', Gtk.main_quit)
+        self._root_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        Gtk.Window.add(self, self._root_box)
+        self._toolbar_box = None
+        self._canvas = None
+
+    def set_canvas(self, widget):
+        if self._canvas is not None:
+            self._root_box.remove(self._canvas)
+        self._canvas = widget
+        self._root_box.pack_start(widget, True, True, 0)
+
+    def set_toolbar_box(self, toolbar_box):
+        if self._toolbar_box is not None:
+            self._root_box.remove(self._toolbar_box)
+        self._toolbar_box = toolbar_box
+        self._root_box.pack_start(toolbar_box, False, False, 0)
+        self._root_box.reorder_child(toolbar_box, 0)
+
+    def get_toolbar_box(self):
+        return self._toolbar_box
+
+    def read_file(self, file_path): pass
+    def write_file(self, file_path): pass
+    def get_activity_root(self): return _ACTIVITY_ROOT
+    def share(self, private=False): pass
